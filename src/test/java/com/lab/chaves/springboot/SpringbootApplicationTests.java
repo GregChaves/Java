a1276890e1b1c8ff0eb638fb3c458cb37169681e
Gregorio Brito Chaves <gbritoch@everis.com>
2021-03-12 08:33:53 -0300

    initial commit

---
+++ src/test/java/com/lab/chaves/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.chaves.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
